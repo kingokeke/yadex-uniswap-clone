214cbcccdd80eaff3bd7bd9b3f1cd670c9f2985b
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-17 07:42:43 +0100

    Added NetworkSelectButton component

---
+++ src/components/Network/NetworkSelectButton.tsx
@@ -0,0 +1,27 @@
+import { BsChevronDown } from "react-icons/bs";
+import networkTypes from "../../data/NetworkTypes";
+import { INetworkType } from "../../models/Interfaces";
+import { NetworkSelectButtonProps } from "../../models/PropTypes";
+
+const NetworkSelectButton = ({
+  activeNetwork,
+  setIsOpen,
+  delay,
+}: NetworkSelectButtonProps) => (
+  <button
+    className="flex items-center bg-white rounded-2xl px-3 py-2 mx-1 font-medium"
+    onMouseEnter={() => setTimeout(() => setIsOpen(true), delay)}
+  >
+    {networkTypes
+      .filter((network: INetworkType) => network.name === activeNetwork)
+      .map((network: INetworkType) => (
+        <span key={network.name} className="flex items-center">
+          <img src={network.image} alt={network.name} className="h-5 mr-2" />
+          {network.name}
+          <BsChevronDown className="ml-2 pt-1" />
+        </span>
+      ))}
+  </button>
+);
+
+export default NetworkSelectButton;
