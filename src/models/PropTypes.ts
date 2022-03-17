6134560c21ebe315ee33329b1abb641aaa5a1b97
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-17 05:22:33 +0100

    Refactored proptypes

--- src/models/PropTypes.ts
+++ src/models/PropTypes.ts
@@ -1,14 +1,20 @@
 import { IMenuLink } from "./Interfaces";
 
-export interface NavbarProps {
+interface BaseProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export interface NavbarProps extends BaseProps {
   links: IMenuLink[];
 }
 
-export interface NavMenuProps {
+export interface NavMenuProps extends BaseProps {
   links: IMenuLink[]
-  className: string;
 }
 
-export interface LogoProps {
-  className: string;
+export interface LogoProps extends BaseProps {
+}
+
+export interface NetworkSelectProps extends BaseProps {
 }
