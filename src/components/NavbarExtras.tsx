6677e456387d192af8dd9e39ea5cb16e395be7e2
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-17 05:55:08 +0100

    Created NavbarExtras component

---
+++ src/components/NavbarExtras.tsx
@@ -0,0 +1,8 @@
+import { FaEllipsisH } from "react-icons/fa";
+import { NavbarExtrasProps } from "../models/PropTypes";
+
+const NavbarExtras = ({ className}: NavbarExtrasProps) => (
+  <button className={`${className} p-2 bg-white px-4 py-2 ml-1 rounded-2xl`}><FaEllipsisH /></button>
+);
+
+export default NavbarExtras;
