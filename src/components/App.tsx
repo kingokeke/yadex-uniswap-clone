9c02bd5af81bf5d2149b8bfb639b4c332e9d37ad
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-17 07:45:24 +0100

    Moved menu data out of App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,9 @@
 import Navbar from "./Navbar";
-import { ILink } from "../models/Interfaces";
 
 const App = () => {
-  const navMenuLinks: ILink[] = [
-    { name: "Swap", url: "swap" },
-    { name: "Pool", url: "pool" },
-    { name: "Vote", url: "vote" },
-    { name: "Charts", url: "charts" },
-  ];
   return (
     <div className="h-screen bg-pink-50">
-      <Navbar links={navMenuLinks} />
+      <Navbar className="" />
     </div>
   );
 }
