6e94db347dacebb1b747d08a6fe99d64382cb846
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-17 05:49:12 +0100

    Created Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import logo from "../assets/images/logo.svg";
+import { LogoProps } from "../models/PropTypes";
+
+const Logo = ({className}: LogoProps) => (
+  <div id="logo" className={className}>
+    <a href="/">
+      <img src={logo} className="h-9" alt="YADEX | Yet Another DEX" />
+    </a>
+  </div>
+);
+
+export default Logo;
