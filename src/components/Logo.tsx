6feac76cdbebb96079d9b3ecd09bdb7d46025cf4
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-18 17:28:26 +0100

    Changed anchor tag to Link

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,10 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.svg";
 import { LogoProps } from "../models/PropTypes";
 
 const Logo = ({className}: LogoProps) => (
   <div id="logo" className={className}>
-    <a href="/">
-      <img src={logo} className="h-9" alt="YADEX | Yet Another DEX" />
-    </a>
+    <Link to="/"><img src={logo} className="h-9" alt="YADEX | Yet Another DEX" /></Link>
   </div>
 );
 
