291879f38624b2deda14ce1b2d1b20ba859e13fb
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-18 17:27:54 +0100

    Added routes for application

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './assets/styles/index.css'
-import App from './components/App'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/styles/index.css";
+import App from "./components/Layout/App";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Swap from "./components/Swap";
+import Pool from "./components/Pool";
+import Vote from "./components/Vote";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Navigate replace to="/swap" />} />
+            <Route path="swap" element={<Swap />} />
+            <Route path="pool" element={<Pool />} />
+            <Route path="vote" element={<Vote />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
