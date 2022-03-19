f5e7845fd595f80062f1fa1f9247229f7be81778
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-19 01:51:50 +0100

    Added functionality to switch tokens

--- src/components/Swap.tsx
+++ src/components/Swap.tsx
@@ -1,14 +1,25 @@
 import { FaArrowDown } from "react-icons/fa";
 import SwapInputField from "./SwapInputField";
-import ethereumIcon from "../assets/images/ethereum.svg";
-import polygonIcon from "../assets/images/polygon.svg";
 import { FiSettings } from "react-icons/fi";
 import { useRecoilState } from "recoil";
-import { currentRouteAtom } from "../store/atoms";
+import { currentRouteAtom, tokenSwapFromAtom, tokenSwapToAtom, walletConnectedAtom } from "../store/atoms";
 import { useEffect } from "react";
 
 const Swap = () => {
   const [_, setCurrentRoute] = useRecoilState(currentRouteAtom);
+  const [walletConnected, setWalletConnected] = useRecoilState(walletConnectedAtom);
+  const [tokenSwapFrom, setTokenSwapFrom] = useRecoilState(tokenSwapFromAtom);
+  const [tokenSwapTo, setTokenSwapTo] = useRecoilState(tokenSwapToAtom);
+
+  const swapButtonText = (
+    !walletConnected ? "Connect Wallet" : "Swap"
+  );
+
+  const switchTokens = () => {
+    const temp = tokenSwapFrom;
+    setTokenSwapFrom(tokenSwapTo);
+    setTokenSwapTo(temp);
+  };
 
   useEffect(() => {
     setCurrentRoute("swap");
@@ -16,20 +27,20 @@ const Swap = () => {
 
   return (
     <div className="flex justify-center mt-20">
-      <div className="p-2 bg-white rounded-2xl w-[480px] shadow-lg">
+      <div className="p-2 bg-white rounded-2xl shadow-lg">
         <div className="flex justify-between items-center px-3 mt-2 mb-4">
           <p className="font-medium text-l">Swap</p>
           <FiSettings className="text-xl cursor-pointer" />
         </div>
-        <SwapInputField token="ETH" icon={ethereumIcon} className="" />
+        <SwapInputField token={tokenSwapFrom} className="" />
         <div className="flex justify-center -mt-4 -mb-4">
-          <div className="flex justify-center items-center w-9 h-9 bg-gray-100 border-white border-4 rounded-xl cursor-pointer z-20">
-            <FaArrowDown/>
+          <div className="flex justify-center items-center w-9 h-9 bg-gray-100 border-white border-4 rounded-xl cursor-pointer z-20" onClick={switchTokens}>
+            <FaArrowDown />
           </div>
         </div>
-        <SwapInputField token="MATIC" icon={polygonIcon} className="" />
+        <SwapInputField token={tokenSwapTo} className="" />
         <div className="">
-          <button className="w-full flex justify-center bg-pink-100 hover:bg-pink-200 py-3 mt-2 rounded-2xl text-pink-500 font-medium text-lg">Connect Wallet</button>
+          <button className="w-full flex justify-center bg-pink-100 hover:bg-pink-200 py-3 mt-2 rounded-2xl text-pink-500 font-medium text-lg">{swapButtonText}</button>
         </div>
       </div>
     </div>
