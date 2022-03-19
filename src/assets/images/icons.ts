517b4af76c2c69fd7bdc3e5c73e8eb7e94f6181b
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-19 01:48:00 +0100

    Created index file for icons

---
+++ src/assets/images/icons.ts
@@ -0,0 +1,13 @@
+import ethereum from "./ethereum.svg";
+import polygon from "./polygon.svg";
+import optimism from "./optimism.svg";
+import arbitrum from "./arbitrum.svg";
+
+const icons = {
+  ethereum,
+  polygon,
+  optimism,
+  arbitrum,
+}
+
+export default icons;
