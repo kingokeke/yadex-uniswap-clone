6bb3aa06067bb9eb8809613fb140d2711d06f4ea
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-22 07:19:50 +0100

    Added DTOs for 1inch dex requests

---
+++ src/models/Moralis/dex-plugin.ts
@@ -0,0 +1,27 @@
+import { MORALIS_CHAIN } from "./core";
+
+interface BaseDTO {
+  chain: MORALIS_CHAIN;
+}
+
+interface TokenDTO extends BaseDTO {
+  fromTokenAddress: string;
+  toTokenAddress: string;
+}
+
+interface TokenAmountDTO extends TokenDTO {
+  amount: string;
+}
+
+export interface IGetSupportedTokensDTO extends BaseDTO {}
+
+export interface IApproveDTO extends TokenDTO {}
+
+export interface IGetQuoteDTO extends TokenAmountDTO {}
+
+export interface IHasAllowanceDTO extends TokenAmountDTO {}
+
+export interface ISwapDTO extends TokenAmountDTO {
+  fromAddress: string,
+  slippage: number;
+}
