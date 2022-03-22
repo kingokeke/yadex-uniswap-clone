b55601a08f51597e7690174d3937b08ef4377ecc
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-22 09:11:42 +0100

    Attempt at bug fix for fetching tokens

--- src/components/Layout/App.tsx
+++ src/components/Layout/App.tsx
@@ -1,20 +1,26 @@
 import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
 import { Outlet } from "react-router-dom";
 import { useRecoilState } from "recoil";
+import { IToken } from "../../models/Interfaces";
+import { IGetSupportedTokensDTO } from "../../models/Moralis/dex-plugin";
 import { tokensListAtom } from "../../store/atoms";
-import { fetchTokensFromServer } from "../../utils/tokenUtils";
+import { sortTokens } from "../../utils/tokenUtils";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const App = () => {
   const fetchTokenDelay = 1000;
+  const { Moralis } = useMoralis();
   const [_, setTokensList] = useRecoilState(tokensListAtom);
 
   useEffect(() => {
     setTimeout(async (): Promise<void> => {
       try {
-        const tokens = await fetchTokensFromServer() as never[];
-        setTokensList(tokens);
+        const requestBody: IGetSupportedTokensDTO = { chain: "eth" };
+        const { tokens } = await Moralis.Plugins.oneInch.getSupportedTokens(requestBody);
+        const tokensList = Object.values<IToken>(tokens).sort((a: IToken, b: IToken) => sortTokens(a, b)) as never[];
+        setTokensList(tokensList);
       } catch (error) {
         console.log("App Component", error);
       }
