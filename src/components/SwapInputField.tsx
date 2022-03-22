86b881121249c3cc85fb32674eb2918a26a3dcfb
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-22 08:52:27 +0100

    Fixed bug with SelectTokenButton props

--- src/components/SwapInputField.tsx
+++ src/components/SwapInputField.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { nullToken } from "../data/frequentlySwappedTokens";
 import { SwapInputFieldProps } from "../models/PropTypes";
 import SelectTokenButton from "./SelectTokenButton";
 import SelectTokenModal from "./SelectTokenModal";
@@ -8,7 +9,7 @@ const SwapInputField = ({ className, token }: SwapInputFieldProps) => {
   return (
     <div className={(className ? className + " " : "") + "bg-gray-100 rounded-2xl border border-solid border-white hover:border-gray-400 flex justify-between"}>
       <input type="text" className="bg-transparent px-4 pt-4 pb-8  w-72 text-3xl font-medium rounded-2xl focus-visible:outline-none" placeholder="0.0" />
-      <SelectTokenButton token={token.symbol ? token : { name: "", symbol: "Select a token", icon: "" }} className={token.symbol ? "bg-gray-50" : "bg-pink-600 text-white"} launchModal={() => setShowModal(true)} />
+      <SelectTokenButton token={token.symbol ? token : { ...nullToken, symbol: "Select a token" }} className={token.symbol ? "bg-gray-50" : "bg-pink-600 text-white"} launchModal={() => setShowModal(true)} />
       <SelectTokenModal className={showModal ? "block" : "hidden"} closeModal={() => setShowModal(false)}/>
     </div>
   );
