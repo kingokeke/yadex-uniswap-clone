cbaba806773cf10cf7d25cd3cfe5db936c677092
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-22 07:16:00 +0100

    Refactored logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,9 @@
-import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.svg";
 import { LogoProps } from "../models/PropTypes";
 
 const Logo = ({className}: LogoProps) => (
   <div id="logo" className={className}>
-    <Link to="/"><img src={logo} className="h-9" alt="YADEX | Yet Another DEX" /></Link>
+    <a href="/"><img src={logo} className="h-9" alt="YADEX | Yet Another DEX" /></a>
   </div>
 );
 
