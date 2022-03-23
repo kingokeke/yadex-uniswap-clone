5dd47c87b17203b52b8d574dc041423520578b88
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2022-03-23 22:42:32 +0100

    Tweaked design of token list to display addresses

--- src/components/TokenListing.tsx
+++ src/components/TokenListing.tsx
@@ -3,10 +3,11 @@ import { TokenListingProps } from "../models/PropTypes";
 const TokenListing = ({ token, className }: TokenListingProps) => {
   return (
     <div className={(className ? className + " " : "") + "flex px-6 py-3 cursor-pointer items-center hover:bg-gray-100"}>
-      <img src={token.logoURI} alt={token.symbol} className="h-6 w-6" />
+      <img src={token.logoURI} alt={token.symbol} className="h-12 w-12" />
       <div className="flex flex-col ml-4">
         <p className="font-medium tracking-wide">{token.symbol}</p>
-        <p className="text-xs text-gray-500">{token.name}</p>
+        <p className="text-xs text-gray-800 font-medium">{token.name}</p>
+        <p className="text-xs text-gray-500">{token.address}</p>
       </div>
     </div>
   );
